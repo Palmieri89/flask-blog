6c673c6b45d8d62900c6cf39d4f8a866541cd080
palmieri89 <andrea.palmieri4@gmail.com>
2023-02-02 10:38:28 +0100

    upload immagini

--- blog/routes.py
+++ blog/routes.py
@@ -25,8 +25,14 @@ def post_create():
     new_post = Post(title=form.title.data, body=form.body.data, slug=slug, 
       description=form.description.data, author=current_user)
     if form.image.data:
-      image = save_picture(form.image.data)
-      new_post.image = image
+      try:
+        image = save_picture(form.image.data)
+        new_post.image = image
+      except Exception:
+        db.session.add(new_post)
+        db.session.commit()
+        flash("C'è stato un problema con l'upload dell'immagine. Cambia Immagine e riprova")
+        return redirect(url_for('post_update',post_id=new_post.id))
     db.session.add(new_post)
     db.session.commit()
     return redirect(url_for('post_detail', post_slug=slug))
@@ -43,6 +49,14 @@ def post_update(post_id):
     post_instance.title = form.title.data
     post_instance.description = form.description.data
     post_instance.body = form.body.data
+    if form.image.data:
+      try:
+        image = save_picture(form.image.data)
+        post_instance.image = image
+      except Exception:
+        db.session.commit()
+        flash("C'è stato un problema con l'upload dell'immagine. Cambia Immagine e riprova")
+        return redirect(url_for('post_update',post_id=post_instance.id))
     db.session.commit()
     return redirect(url_for('post_detail', post_slug=post_instance.slug))
   elif request.method == "GET":
