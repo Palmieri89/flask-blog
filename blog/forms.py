bc8f09b4b5d84232fc79d6b62700018c9f75ffe7
palmieri89 <andrea.palmieri4@gmail.com>
2023-02-10 10:39:38 +0100

    Created form signup and modified remember token datatime and

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField, EmailField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from blog.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', 
@@ -21,3 +22,30 @@ class PostForm(FlaskForm):
     image = FileField('Copertina Articolo', 
         validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
     submit = SubmitField('Pubblica Post')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username',
+      validators=[DataRequired("Campo Obligatorio"), Length(8, 64),
+                  Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,'Usernames must have only letters, ''numbers, dots or underscores')])
+    password = PasswordField('Password',
+      validators=[DataRequired("Campo Obbligatorio"), EqualTo ('confirmpassword', message='Passwords must match.')])
+    confirmpassword = PasswordField('Conferma Password', 
+      validators=[DataRequired("Campo Obbligatorio")])
+    email = EmailField('Email', validators=[DataRequired("Campo Obbligatorio"), Length(1, 64), Email()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+ 
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+  #Il validator usato nel campo “username”, si assicura che vengano utilizzati solo
+  #lettere, numeri, punti e underscores altrimenti ritorna il messaggio di errore,
+  #passato come argomento a Regexp.
+  #Nel campo password abbiamo un altro validator (EqualTo) che si assicura che le
+  #password immesse (password e conferma della stessa) siano uguali.
+  #I due metodi validate_email() e validate_username() si assicurano che i valori
+  #immessi nei campi non siano già presenti a database, sollevando in caso affermativo,
+  #l’eccezione di competenza.    
--- blog/routes.py
+++ blog/routes.py
@@ -2,9 +2,10 @@ from flask import abort, flash, redirect,  render_template, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 
 from blog import db, app
-from blog.forms import LoginForm, PostForm
+from blog.forms import LoginForm, SignupForm, PostForm
 from blog.models import Post, User
 from blog.utilis import save_picture, title_slugifier
+
 
 @app.route("/")
 def homepage():
@@ -22,7 +23,7 @@ def post_create():
   form = PostForm()
   if form.validate_on_submit():
     slug = title_slugifier(form.title.data)
-    new_post = Post(title=form.title.data, body=form.body.data, slug=slug, 
+    new_post = Post(title=form.title.data.strip(), body=form.body.data, slug=slug, 
       description=form.description.data, author=current_user)
     if form.image.data:
       try:
@@ -46,7 +47,7 @@ def post_update(post_id):
     abort(403)
   form = PostForm()
   if form.validate_on_submit():
-    post_instance.title = form.title.data
+    post_instance.title = form.title.data.strip()
     post_instance.description = form.description.data
     post_instance.body = form.body.data
     if form.image.data:
@@ -60,7 +61,7 @@ def post_update(post_id):
     db.session.commit()
     return redirect(url_for('post_detail', post_slug=post_instance.slug))
   elif request.method == "GET":
-    form.title.data = post_instance.title
+    form.title.data = post_instance.title.strip()
     form.description.data = post_instance.description
     form.body.data = post_instance.body
   return render_template("post_editor.html", form=form)
@@ -79,14 +80,28 @@ def post_delete(post_id):
 def about():
   return render_template("about_page.html")
 
+@app.route("/Signup", methods=["GET", "POST"])
+def Signup():
+  form = SignupForm()
+  if form.validate_on_submit():
+    new_user = User(username=form.username.data.strip(), 
+                    email=form.email.data, 
+                    password=form.password.data)
+    new_user.set_password_hash(new_user.password) 
+    db.session.add(new_user)
+    db.session.commit()
+    flash('You can now login.')
+    return redirect(url_for('login'))
+  return render_template("Signup.html", form=form)
+    
 @app.route("/login", methods=["GET", "POST"])
 def login():
   if current_user.is_authenticated:
     return redirect(url_for('homepage'))
   form = LoginForm()
   if form.validate_on_submit():
-    user = User.query.filter_by(username=form.username.data).first()
-    if user is None or not user.check_password(form.password.data):
+    user = User.query.filter_by(username=form.username.data.strip()).first()
+    if user is None or not user.check_password(form.password.data.strip()):
       flash('username e password non coincidono!')
       return redirect(url_for('login'))
     login_user(user, remember=form.remember_me.data)
